bcc78e573473776318065169b44c48c7d5932cac
fati <faateme.ghasemii@gmail.com>
2024-05-18 17:05:08 -0400

    test_patch

--- test_patch.py
+++ test_patch.py
@@ -5,17 +5,15 @@ import random
 
 
 def test_response(playwright: sync_playwright):
-    email = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    
+
     with open('./JsonFile/CreateUser.json', 'r') as read_file:
         data = json.load(read_file)
-        data['email'] = "fati.gh"+email+"1@15ce.com"
 
     context = playwright.request.new_context(base_url="https://gorest.co.in")
-    response = context.post(url="/public/v2/users", headers={
+    response = context.patch(url="/public/v2/users/6914330", headers={
         "Authorization": "Bearer 86152c35d129b4285010db11d60a7377e34d201c4092054db0124d16a9ed2fd2"
     }, data=data)
-    res = response.json()
 
-    assert response.status == 201
-    assert res.get("email") == "fati.gh"+email+"1@15ce.com"
+    print(response.body())
+
+    assert response.status == 200
