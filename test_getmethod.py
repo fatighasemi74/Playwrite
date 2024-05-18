e71c19ee4e4b9785d9eeabfd039c32d160bbf152
fati <faateme.ghasemii@gmail.com>
2024-05-18 11:58:00 -0400

    test_getmethod.py created

---
+++ test_getmethod.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import sync_playwright
+
+
+def test_get(playwright: sync_playwright):
+    context = playwright.request.new_context()
+    response = context.get(url="https://gorest.co.in/public/v2/users/2934569", headers={
+        "Authorization": "Bearer 86152c35d129b4285010db11d60a7377e34d201c4092054db0124d16a9ed2fd2"
+    })
+    res = response.json()
+
+    assert response.status == 200
+    assert response.status_text == 'OK'
+    assert response.json()["name"] == "Tarun Johar"
+    assert 
