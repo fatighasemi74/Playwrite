e5c15caeed8b54084ddd56d8619465c4484c3398
fati <faateme.ghasemii@gmail.com>
2024-05-18 16:34:34 -0400

    test_post

--- test_post.py
+++ test_post.py
@@ -1,15 +1,23 @@
+import string
 from playwright.sync_api import sync_playwright
+import random
 
 
 def test_response(playwright: sync_playwright):
-    query_params = {
-        "page": "1"
+    email = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    data = {
+        "name": "fati gh",
+        "gender": "male",
+        "email": "fati.gh"+email+"1@15ce.com",
+        "status": "active"
     }
+    
+
     context = playwright.request.new_context(base_url="https://gorest.co.in")
-    response = context.get(url="/public/v2/users", params=query_params, headers={
+    response = context.post(url="/public/v2/users", headers={
         "Authorization": "Bearer 86152c35d129b4285010db11d60a7377e34d201c4092054db0124d16a9ed2fd2"
-    })
+    }, data=data)
     res = response.json()
 
-    assert response.status == 200
-    assert response.status_text == 'OK'
+    assert response.status == 201
+    assert res.get("email") == "fati.gh"+email+"1@15ce.com"
