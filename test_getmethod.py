20da74eab945531f80b3a2436b2fb646f3e1302a
fati <faateme.ghasemii@gmail.com>
2024-05-18 12:02:13 -0400

    assert headers

--- test_getmethod.py
+++ test_getmethod.py
@@ -3,12 +3,13 @@ from playwright.sync_api import sync_playwright
 
 def test_get(playwright: sync_playwright):
     context = playwright.request.new_context()
-    response = context.get(url="https://gorest.co.in/public/v2/users/2934569", headers={
+    response = context.get(url="https://gorest.co.in/public/v2/users/2934570", headers={
         "Authorization": "Bearer 86152c35d129b4285010db11d60a7377e34d201c4092054db0124d16a9ed2fd2"
     })
     res = response.json()
 
     assert response.status == 200
     assert response.status_text == 'OK'
-    assert response.json()["name"] == "Tarun Johar"
-    assert 
+    assert response.json()["name"] == "Aamod Joshi"
+    assert res.get("name") == "Aamod Joshi"
+    assert response.headers["content-type"] == "application/json; charset=utf-8"
