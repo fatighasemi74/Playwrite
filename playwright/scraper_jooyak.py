4681880b4e1a3e097e35b76053b5545aaefe3f56
fati <faateme.ghasemii@gmail.com>
2024-05-21 21:33:56 -0400

    click by roles

--- playwright/scraper_jooyak.py
+++ playwright/scraper_jooyak.py
@@ -4,14 +4,20 @@ from playwright.sync_api import sync_playwright
 def test_website():
     with sync_playwright() as p:
         # launch a browser
-        browser = p.chromium.launch(headless=False, slow_mo=500)
+        browser = p.chromium.launch(headless=False, slow_mo=1000)
         # create a new page
         page = browser.new_page()
         base_url = "https://jooyak.netlify.app/"
-
-        # locate a link element with "Blogg" text
         page.goto(base_url)
-        # shop_button = page.get_by_role('link', name="Blogg")
-        # shop_button.click()
+        
+        # locate a link element with " Suggest it " text
+        button = page.get_by_role('button', name="Suggest it")
+        button.highlight()
+        button.click()
+
+        # Books & Audible
+        heading = page.get_by_role('heading', name="Books & Audible")
+        heading.highlight()
+        heading.click()
 
         browser.close()    
